f89fce8265d6c8e4b708c711816e2b6c4fad1572
Adrian Gajda <adrian.gajda@hotmail.com>
2022-10-12 22:34:45 +0100

    CR

--- src/hooks/useCartReducer.tsx
+++ src/hooks/useCartReducer.tsx
@@ -42,81 +42,87 @@ export interface IAction {
 
 // redux-toolkit -> slice
 
-export default function useCartReducer() {
-  const reducer = (state: IBasketState, action: IAction) => {
-    const isBasketEmpty = state.basketContent.length === 0;
-    const newProduct = action.product;
-    const qtyChange = action.qty;
+const reducer = (state: IBasketState, action: IAction, initialState) => {
+  const isBasketEmpty = state.basketContent.length === 0;
+  const newProduct = action.product;
+  const qtyChange = action.qty;
 
-    // spread <-------------
-    switch (action.type) {
-      case "updateCart":
-        const isInBasket = state.basketContent.some(
-          (elem: IBasketContent) => elem.id === newProduct.id
-        );
-        if (!isInBasket)
-          return {
-            // immer.js
-            ...state,
-            basketContent: [
-              ...state.basketContent,
-              { ...action.product, quantity: 1 },
-            ],
-          };
-
-        //   Check if not exceeding stock
-        const productQuantityInBasket = state.basketContent.filter(
-          (elem: IBasketContent) => elem.id === newProduct.id
-        )[0].quantity;
-
-        if (productQuantityInBasket >= newProduct.quantity && qtyChange === 1)
-          return state;
-
-        // else update quantity
-
-        // if current basket quantity is 1 remove it from basket after decreasing quantity
-        if (
-          state.basketContent.filter(
-            (elem: IBasketContent) => elem.id === newProduct.id
-          )[0].quantity === 1 &&
-          qtyChange === -1
-        ) {
-          const filteredState = state.basketContent.filter(
-            (item: IBasketContent) => item.id !== newProduct.id
-          );
-          return { ...state, basketContent: filteredState };
-        }
-
+  // spread <-------------
+  switch (action.type) {
+    case "updateCart":
+      const isInBasket = state.basketContent.some(
+        (elem: IBasketContent) => elem.id === newProduct.id
+      );
+      if (!isInBasket)
         return {
-          basketContent: state.basketContent.map((item: IBasketContent) => {
-            if (item.id === newProduct.id) {
-              const newQuantity = item.quantity + qtyChange;
-              return { ...item, quantity: newQuantity };
-            }
-            return item;
-          }),
+          // immer.js
+          ...state,
+          basketContent: [
+            ...state.basketContent,
+            { ...action.product, quantity: 1 },
+          ],
         };
 
-      case "remove_all":
-        if (isBasketEmpty) return state;
+      // bigOnotation  n * 4  / space1
+      // state.basketContent.length * 4
 
-        return initialState;
+      //   Check if not exceeding stock
+      const productQuantityInBasket = state.basketContent.filter(
+        (elem: IBasketContent) => elem.id === newProduct.id
+      )[0].quantity;
 
-      default:
+      if (productQuantityInBasket >= newProduct.quantity && qtyChange === 1)
         return state;
-    }
-  };
-  const initialState = { basketContent: [] };
 
-  const [state, dispatch] = useReducer(reducer, initialState);
+      // else update quantity
 
+      // if current basket quantity is 1 remove it from basket after decreasing quantity
+      if (
+        state.basketContent.filter(
+          (elem: IBasketContent) => elem.id === newProduct.id
+        )[0].quantity === 1 &&
+        qtyChange === -1
+      ) {
+        const filteredState = state.basketContent.filter(
+          (item: IBasketContent) => item.id !== newProduct.id
+        );
+        return { ...state, basketContent: filteredState };
+      }
+
+      return {
+        basketContent: state.basketContent.map((item: IBasketContent) => {
+          if (item.id === newProduct.id) {
+            const newQuantity = item.quantity + qtyChange;
+            return { ...item, quantity: newQuantity };
+          }
+          return item;
+        }),
+      };
+
+    case "remove_all":
+      if (isBasketEmpty) return state;
+
+      return initialState;
+
+    default:
+      return state;
+  }
+};
+
+const usePrepareCartActions = (dispatch) => {
   const removeAllProducts = () => {
-    dispatch({ type: "remove_all" });
+    dispatch({ type: "REMOVE_ALL" });
   };
 
   const updateBasket = (product: any, newQty: number) => {
     dispatch({ type: "updateCart", product: product, qty: newQty });
   };
+};
+
+export default function useCartReducer() {
+  const initialState = { basketContent: [] };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return {
     removeAllProducts,
